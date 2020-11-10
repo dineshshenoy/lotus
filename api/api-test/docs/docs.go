3316efebd53ccc5ffc2efb8fb4f105006e316b7a
Dinesh Shenoy <dinesh.shenoy@protocol.ai>
2020-11-09 17:23:21 -0800

    Added general swagger info

---
+++ api/api-test/docs/docs.go
@@ -0,0 +1,24 @@
+// Package classification awesome.
+//
+// Documentation of our awesome API.
+//     BasePath: /rpc/v0
+//     Schemes: http
+// 
+//     Version: 1.0.2
+//     Host: <lotus-node>:1234
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - basic
+//
+//    SecurityDefinitions:
+//    basic:
+//      type: basic
+//
+// swagger:meta
+package docs
